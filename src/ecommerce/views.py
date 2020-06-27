0196420a25aaf59da95681115906ac7f9805cb4d
mr.dosjung <mr.dosjung@gmail.com>
2020-06-27 21:01:25 +0900

    settings.py에 templates DIR 추가 및 contact_page, about_page 등 기본 html 렌더링 및 bootstrap html 기본 페이지 작성

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -2,6 +2,27 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
+    context = {
+        "title": "Hello World",
+        "content": " Welcome to the homepage."
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Contact Page",
+        "content": " Welcome to the contact page"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "About Page",
+        "content": " Welcome to the about page"
+    }
+    return render(request, "home_page.html", context)
+
+def home_page_old(request):
     html_ = """
             <!doctype html>
             <html lang="en">
@@ -29,3 +50,4 @@ def home_page(request):
             </html>    
     """
     return HttpResponse(html_)
+
