002e38ba89b1b6602acd646ed98eed29c4b5038a
mr.dosjung <mr.dosjung@gmail.com>
2020-06-27 21:23:58 +0900

    html에 기본 form추가 및 post로 정보 전송, 장고views.py에서 해당 정보 출력

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -13,7 +13,12 @@ def contact_page(request):
         "title": "Contact Page",
         "content": " Welcome to the contact page"
     }
-    return render(request, "home_page.html", context)
+    if request.method == 'POST':
+        print(request.POST)
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
+    return render(request, "contact/views.html", context)
 
 def about_page(request):
     context = {
