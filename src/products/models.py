a47952d6fce45a70999b22a16b565b45120733d7
mr.dosjung <mr.dosjung@gmail.com>
2020-07-03 02:45:11 +0900

    ListView, DetailView 추가

--- src/products/models.py
+++ src/products/models.py
@@ -5,4 +5,11 @@ from django.db import models
 class Product(models.Model): # CamelCase로 작성하며, 단수로 처리하는것이 Convention임. 즉 Products 이렇게 하지 않는다.
     title = models.CharField(max_length=120)
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=20, default=39.00) # 또는 null=True로 해도 됨.
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99) # 또는 null=True로 해도 됨.
+
+    def __str__(self):  # python3
+        return self.title
+
+    def __unicode__(self):  # this is 하위호환성. python2에서는 이렇게 사용해야함.
+        return self.title
+
--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+# 이렇게 해도 된다. generic에 init.py에 shortcut이 있으므로,
+# from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product
+
 
 # Create your views here.
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    # 참고로 Class based view 에서 default view 이름은 product(모델명소문자)_list.html이다.
+    # 따라서 template_name = "products/list.html" 로 하고 아무것도 안만들면,
+    # product_list.html, list.html 2개의 template이 없다는 에러가 나온다. (Tip!!)
+    # 실제 ListView 소스를 보면 template을 못찾는 경우, get_template_names 를 호출해서 default template을 찾는다.
+    template_name = "products/list.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
+
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/list.html", context)
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    # 참고로 Class based view 에서 default view 이름은 product(모델명소문자)_list.html이다.
+    # 따라서 template_name = "products/list.html" 로 하고 아무것도 안만들면,
+    # product_list.html, list.html 2개의 template이 없다는 에러가 나온다. (Tip!!)
+    # 실제 ListView 소스를 보면 template을 못찾는 경우, get_template_names 를 호출해서 default template을 찾는다.
+    template_name = "products/detail.html"
+    hello_words = "Hello, World welcome to django world."
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    # instance = Product.objects.get(pk=pk)
+    instance = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
