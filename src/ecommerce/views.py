73f78677355ab1c0d3fe9b023a9d98f5b35de2d8
dongsoon.jung <dongsoon.jung@deliveryhero.co.kr>
2021-01-04 01:43:15 +0900

    auto search 및 contact form ajax 적용완료

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 
 from django.contrib.auth import authenticate, login, get_user_model
@@ -25,6 +25,15 @@ def contact_page(request):
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
+        if request.is_ajax():
+            return JsonResponse({"message": "Thank you for your submission."})
+
+    if contact_form.errors:
+        errors = contact_form.errors.as_json()
+        print("server log:"+str(errors))
+        if request.is_ajax():
+            return HttpResponse(errors, status=400, content_type='application/json')
+
     # if request.method == 'POST':
     #     print(request.POST)
     #     print(request.POST.get('fullname'))
